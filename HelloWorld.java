8504e326d994b15952c8d65211855ef0211edea6
Marcos Shirafuchi <marcosfshirafuchi@gmail.com>
2023-10-20 15:40:48 -0300

    Primeiro commit de Fundamentos Java!

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * HelloWorld = Nome da minha classe
+ * public = Tipo de acesso da classe
+ * class = Tipo da classe
+ */
+public class HelloWorld{
+    // Todo conteúdo deverá ser inserido aqui dentro
+    public static void main(String[] args){
+        /*
+         * Valores (int, double, float, long)
+         * Texto (String)
+         * Boleano (boolean)
+         */
+        int dadoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 8934723423949L;
+        String dadoDoTipoString = "Colocar o meu texto";
+        boolean dadoDoTipoBoolean = false;
+        
+    }
+}
+// Fora do escopo da classe
